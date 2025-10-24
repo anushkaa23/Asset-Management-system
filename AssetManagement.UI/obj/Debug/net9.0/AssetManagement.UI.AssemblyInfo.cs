59ef6f17fe4b667d6cd2bfa61911dbf18f5290c3
anushkaa23 <anushka21_ug@cse.nits.ac.in>
2025-10-24 06:11:56 +0530

    Add pending database migrations

--- AssetManagement.UI/obj/Debug/net9.0/AssetManagement.UI.AssemblyInfo.cs
+++ AssetManagement.UI/obj/Debug/net9.0/AssetManagement.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetManagement.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29089d3b636d68f8d4796fa1f95685f50c6b13e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6485ac99b72039be7a892694dd5db10474445714")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetManagement.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetManagement.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
